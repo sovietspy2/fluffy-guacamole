fb078ccf3c244d702290680c8c89dd2593714717
SovietSpy2 <sovietspy2@yandex.com>
2019-06-12 15:16:25 +0200

    added npm commands

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.listen( 8080,()=> console.log("Server started"));
+app.listen(8080, () => console.log("Server started"));
 
-app.get("/test", (req, res)=> {
-    console.log("RQ");
-    res.send("HELO THERE");
-})
+app.get("/test", (req, res) => {
+  console.log("RQ");
+  res.send("HELO THERE");
+});
+
+app.use("/", express.static(path.join(__dirname, "frontend/build")));
+// serving the react frontend with method we can avoid CORS problems in production enviornment
